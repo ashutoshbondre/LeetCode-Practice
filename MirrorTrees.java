bc87524c4e29cad31e327cab5e9280694ba471a4
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-04-30 11:45:31 +0530

    Add files via upload

---
+++ MirrorTrees.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return helper(root.left,root.right);
+    }
+    public boolean helper(TreeNode left, TreeNode right)
+    {
+        if(left==null || right==null) return left==right;
+        if(left.val!=right.val) return false;
+        return(helper(left.left,right.right) && helper(left.right,right.left));
+    }
+}
