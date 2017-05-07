610189d2c5b7328c983664a975d3c4638ccc67c2
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-05-07 22:24:20 +0530

    Update IntersectionOfTwoArrays.java

--- IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays.java
@@ -5,7 +5,8 @@ public class Solution {
         for (int i = 0; i < nums1.length; i++) {
             set.add(nums1[i]);
         }
-        for (int i = 0; i < nums2.length; i++) {
+        for (int i = 0; i < nums2.length; i++) 
+        {
             if (set.contains(nums2[i])) {
                 intersect.add(nums2[i]);
             }
