6dc6a9f51613cf1c5b3e3881ccfb433408e0b2c2
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-05-23 23:54:15 +0530

    Add files via upload

---
+++ BinaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue <TreeNode> q = new LinkedList<TreeNode>();
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        if(root==null) return result;
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            int level = q.size();
+            List<Integer> sublist = new ArrayList<Integer>();
+            for(int i=0;i<level;i++)
+            {
+                if(q.peek().left!=null) q.offer(q.peek().left);
+                if(q.peek().right!=null) q.offer(q.peek().right);
+                sublist.add(q.poll().val);
+            }
+            result.add(0,sublist);
+        }
+        return result;
+    }
+}
