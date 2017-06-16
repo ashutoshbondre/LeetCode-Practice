7c6dff60fc08a33c1fcb4b318cbd7f5efc9f1a3c
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-06-15 17:35:51 -0700

    Add files via upload

---
+++ RotateImage.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public void rotate(int[][] matrix) {
+        for (int row=0;row<matrix.length;row++)
+        {
+            for(int col=row;col<matrix[0].length;col++)
+            {
+                int temp = 0;
+                temp=matrix[row][col];
+                matrix[row][col]=matrix[col][row];
+                matrix[col][row]=temp;
+            }
+            
+        }
+        for(int row=0;row<matrix.length;row++)
+        {
+            for(int col=0;col<matrix.length/2;col++)
+            {
+                int temp=0;
+                temp=matrix[row][col];
+                matrix[row][col]=matrix[row][matrix.length-1-col];
+                matrix[row][matrix.length-1-col] =temp;
+            }
+        }
+    }
+}
