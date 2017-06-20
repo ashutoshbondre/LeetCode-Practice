63b20dbabc8c7c9bcb61d69016eebb6a42c359e9
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-06-20 00:44:22 -0700

    Add files via upload

---
+++ TwoSumII.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int [] res = new int[2];
+        if(numbers==null || numbers.length<2) return res;
+        int left=0;
+        int right=numbers.length-1;
+        while(left<right)
+        {
+            int sum=numbers[left] + numbers[right];
+            if(sum==target) 
+            {
+                res[0]=left+1;
+                res[1]=right+1;
+                break;
+            }
+            else if(sum<target)
+            left++;
+            else
+            right--;
+        }
+        return res;
+    }
+}
