5eb5a0bfd4bf15eda52c526f50de85d2d628b9cc
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-06-25 15:44:39 -0700

    Add files via upload

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,25 +1,28 @@
 public class Solution {
-private int lo, maxLen;
-
-public String longestPalindrome(String s) {
-	int len = s.length();
-	if (len < 2)
-		return s;
-	
-    for (int i = 0; i < len-1; i++) {
-     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-     	extendPalindrome(s, i, i+1); //assume even length.
+    private int lo;
+    private int maxLen;
+    public String longestPalindrome(String s) 
+    {
+        if(s.length()<2)return s;
+        for(int i=0;i<s.length();i++)
+        {
+            extendPalindrome(s,i,i);
+            extendPalindrome(s,i,i+1);
+        }
+        return s.substring(lo,lo+maxLen);
     }
-    return s.substring(lo, lo + maxLen);
+    
+    public void extendPalindrome(String s,int j,int k)
+    {
+        while(j>=0 && k<s.length() && s.charAt(j)==s.charAt(k))
+        {
+            j--;
+            k++;
+        }
+        if(maxLen<k-j-1)
+        {
+            lo=j+1;
+            maxLen=k-j-1;
+        }
+    }
 }
-
-private void extendPalindrome(String s, int j, int k) {
-	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-		j--;
-		k++;
-	}
-	if (maxLen < k - j - 1) {
-		lo = j + 1;
-		maxLen = k - j - 1;
-	}
-}}
