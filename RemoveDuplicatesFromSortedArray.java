aca5df697009f303da87aacfca5a3b36109b0f19
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-06-27 20:05:22 -0700

    Add files via upload

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n =nums.length;
+        if(n<2)return n;
+        int id=1;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]!=nums[i-1])
+                nums[id++]=nums[i];
+        }
+        return id;
+    }
+}
