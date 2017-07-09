70168709a60284b79c97f09d8dbbc5bdf59effc8
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-07-08 21:09:56 -0700

    Add files via upload

---
+++ DeleteNodeinABST.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return null;
+        if(key<root.val)    root.left=deleteNode(root.left,key);
+        else if(key>root.val)  root.right=deleteNode(root.right,key);
+        else
+        {
+            if(root.left==null && root.right==null) return null;
+            if(root.left==null) return root.right;
+            if(root.right==null)return root.left;
+            else
+            {
+                TreeNode min = findMin(root.right);
+                root.val=min.val;
+                root.right=deleteNode(root.right,root.val);
+        }
+    }
+        return root;
+}
+    
+    public TreeNode findMin(TreeNode node)
+    {
+        while(node.left!=null)
+        {
+            node=node.left;
+        }
+        return node;
+    }
+}
