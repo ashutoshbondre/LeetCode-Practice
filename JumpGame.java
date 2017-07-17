f7d459070b48eef7e488811c19d279602669e371
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-07-16 22:17:33 -0700

    Add files via upload

--- JumpGame.java
+++ JumpGame.java
@@ -1,8 +1,48 @@
-public boolean canJump(int[] A) {
-    int max = 0;
-    for(int i=0;i<A.length;i++){
-        if(i>max) {return false;}
-        max = Math.max(A[i]+i,max);
+public class Solution {
+    public boolean canJump(int[] nums) {
+     int max = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i>max) {return false;}
+            max=Math.max(nums[i]+i,max);
+        }
+        return true;
     }
-    return true;
 }
+
+/*
+Dynamic Programming Solution
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+public class Solution {
+    Index[] memo;
+
+    public boolean canJumpFromPosition(int position, int[] nums) {
+        if (memo[position] != Index.UNKNOWN) {
+            return memo[position] == Index.GOOD ? true : false;
+        }
+
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (canJumpFromPosition(nextPosition, nums)) {
+                memo[position] = Index.GOOD;
+                return true;
+            }
+        }
+
+        memo[position] = Index.BAD;
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+        return canJumpFromPosition(0, nums);
+    }
+}
+*/
