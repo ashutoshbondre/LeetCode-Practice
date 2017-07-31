7c7e3bd652dba8185de97bc8d351054d74a6971c
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-07-30 19:40:10 -0700

    Add files via upload

---
+++ InvertTree.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+    public void invert (TreeNode root)
+    {
+        if(root==null) return;
+        TreeNode temp = root.left;
+        root.left=root.right;
+        root.right=temp;
+        invert(root.left);
+        invert(root.right);
+    }
+}
