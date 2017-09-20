08e36d5170bdfc4865ebb387f9f63f22a448cc0c
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-09-19 21:54:38 -0400

    Add files via upload

--- ClosestBinarySearchTreeValue.java
+++ ClosestBinarySearchTreeValue.java
@@ -1,14 +1,27 @@
-public int closest (TreeNode root, double target)
-{
-	int ret = root.val;
-	while(root!=null)
-	{
-		if(Math.abs(root.val - target) < Math.abs(ret.val-target))
-		{
-			ret=root.val;
-		}
-		root = root.val<target?root.right:root.left;
-
-	}
-	return ret;
+class Solution {
+    public int closestValue(TreeNode root, double target) {
+    int ret = root.val;
+        while(root!=null)
+        {
+            if(Math.abs(root.val-target)<Math.abs(ret-target))
+            {
+                ret = root.val;
+            }
+            root = root.val>target? root.left:root.right;
+        }
+        return ret;
 }
+}
+
+
+/*Recursive:
+
+    public int closestValue(TreeNode root, double target) {
+        int a  = root.val;
+        TreeNode kid = target<a ? root.left:root.right;
+        if(kid==null) return a;
+        int b = closetValue(kid,target);
+        return Math.abs(a-target) < Math.abs(b-target) ? a:b;
+    }
+  
+*/
