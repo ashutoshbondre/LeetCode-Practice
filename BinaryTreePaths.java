b1c8589065f018539c5180c606b4d7238b675b7e
Ashutosh Bondre <ashutoshbondre@gmail.com>
2017-10-18 23:40:02 -0400

    Update BinaryTreePaths.java

--- BinaryTreePaths.java
+++ BinaryTreePaths.java
@@ -4,7 +4,8 @@ public List<String> binaryTreePaths(TreeNode root) {
     if (root != null) searchBT(root, "", answer);
     return answer;
 }
-private void searchBT(TreeNode root, String path, List<String> answer) {
+private void searchBT(TreeNode root, String path, List<String> answer) 
+{
     if (root.left == null && root.right == null) answer.add(path + root.val);
     if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
     if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
