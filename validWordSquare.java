48a58502d3355c13c0f57c0db157f576f6a9e249
Ashutosh Bondre <ashutoshbondre@gmail.com>
2018-01-13 12:27:44 -0800

    Add files via upload

---
+++ validWordSquare.java
@@ -0,0 +1,17 @@
+public class Solution{
+	public boolean isValidSquare(List<String> words)
+	{
+		if(words==null|| words.size()==0)
+			return true;
+		int n = words.size();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<words.get(i).length();j++)
+			{
+				if(j>=n || words.get(j).length()<=i || words.get(i).charAt(j)!=words.get(j).charAt(i))
+					return false;
+			}
+		}
+		return true;
+	}
+}
