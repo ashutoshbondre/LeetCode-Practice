66f61b52afc22ac431e4bd1750938ce62565763b
Ashutosh Bondre <ashutoshbondre@gmail.com>
2018-01-17 17:21:22 -0800

    Add files via upload

---
+++ WiggleSort.java
@@ -0,0 +1,26 @@
+public class Solution{
+	public void wiggleSort(int [] nums)
+	{
+		for(int i=0;i<nums.length;i++)
+		{
+			if(i%2==1)
+			{
+				if(nums[i-1]>nums[i])
+					swap(nums,i);
+			}
+			else
+			{
+				if(i!=0 && nums[i-1]<nums[i])
+					swap(nums,i);
+			}
+		}
+
+	}
+
+	private void swap(int []nums,int i)
+	{
+		int temp = nums[i];
+		nums[i]=nums[i-1];
+		nums[i-1]=temp;
+	}
+}
